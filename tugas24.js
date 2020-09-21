15f5139ad8a4ff7c3fafaf6a7be0bce66f0db0c5
tasyaSukma <maftuhah.tasya@gmail.com>
2020-09-21 11:28:29 +0700

    Add files via upload

---
+++ tugas24.js
@@ -0,0 +1,5 @@
+var num = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66]
+
+console.log("Sebelumnya : ", num.toString())
+console.log("Ascending : ", num.sort().toString())
+console.log("Descending : ", num.reverse().toString())
